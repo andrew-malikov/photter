351d33042a134ef06e256ed39f3426bc26b02712
andrew <andrew.devels@gmail.com>
2019-03-30 21:54:17 +0500

    fear(Config): provide the db connection string

--- Photter.Configs/ProjectConfig.cs
+++ Photter.Configs/ProjectConfig.cs
@@ -1,6 +1,11 @@
+using Newtonsoft.Json;
+
 namespace Photter.Configs {
     public class ProjectConfig {
         public readonly string AppId;
+
+        [JsonIgnore]
+        public readonly string DbConnetion = "Data Source=photter.db";
 
         public ProjectConfig(string appId) {
             AppId = appId;
